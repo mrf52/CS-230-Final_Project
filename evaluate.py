5da000acc2a517b18f54286b845402875bf240e1
gcasarez <gcasarez@stanford.edu>
2022-12-06 21:15:53 +0000

    adding test set and confusion matrix

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import seaborn as sns
 
 
-def plot_accuracy_loss(acc, val_acc, loss, val_loss):
+def plot_accuracy_loss(acc, val_acc, loss, val_loss, name='accuracy_and_loss.png'):
     plt.figure(figsize=(8, 8))
     plt.subplot(2, 1, 1)
     plt.plot(acc, label='Training Accuracy')
@@ -19,4 +22,21 @@ def plot_accuracy_loss(acc, val_acc, loss, val_loss):
     # plt.ylim([0,1.0])
     plt.title('Training and Validation Loss')
     plt.xlabel('epoch')
-    plt.savefig("accuracy_and_loss.png")
+    plt.savefig(name)
+
+
+def plot_confusion_matrix(dataset, model, name='confusion_matrix.png'):
+    gt_labels = np.concatenate([x for x, y in dataset], axis=0)
+    gt_labels = gt_labels.argmax(axis=1)
+    predicted_labels = model.predict(dataset).argmax(axis=1)
+    confusion = tf.math.confusion_matrix(labels=gt_labels, predictions=predicted_labels, num_classes=5)
+    plt.figure(figsize=(5, 5))
+    sns.heatmap(confusion, annot=True, fmt="d")
+    plt.savefig(name)
+
+
+def evaluate_model(dataset, model):
+    images = np.concatenate([x for x, y in dataset], axis=0)
+    labels = np.concatenate([y for x, y in dataset], axis=0)
+    model.evaluate(images, labels)
+
--- execute_training_pipeline.py
+++ execute_training_pipeline.py
@@ -2,12 +2,15 @@ from train import train
 from generate_datasets import generate_datasets
 from preprocess import preprocess
 from model import animal_model
-from evaluate import plot_accuracy_loss
+from evaluate import plot_accuracy_loss, plot_confusion_matrix, evaluate_model
 import argparse
 
 
-def run(directory, img_size, batch_size, initial_epochs, fine_tune_epochs, base_lr):
-    train_dataset, validation_dataset = generate_datasets(directory, img_size=img_size, batch_size=batch_size)
+def run(directory, test_directory, img_size, batch_size, initial_epochs, fine_tune_epochs, base_lr,
+        validation_split):
+    train_dataset, validation_dataset, test_dataset = generate_datasets(directory, test_directory,
+                                                                        img_size=img_size, batch_size=batch_size,
+                                                                        validation_split=validation_split)
     print("Generated datasets")
     data_augmentation, preprocess_input = preprocess(train_dataset)
     model = animal_model(img_size, data_augmentation, preprocess_input)
@@ -16,27 +19,36 @@ def run(directory, img_size, batch_size, initial_epochs, fine_tune_epochs, base_
                                          fine_tune_epochs=fine_tune_epochs, base_learning_rate=base_lr)
     print("Trained model")
     plot_accuracy_loss(acc, val_acc, loss, val_loss)
+    plot_confusion_matrix(test_dataset, model, name='confusion_matrix_test.png')  # confusion for test dataset
+    plot_confusion_matrix(train_dataset, model, name='confusion_matrix_train.png')  # confusion for train dataset
+    plot_confusion_matrix(validation_dataset, model, name='confusion_matrix_val.png')  # confusion for val dataset
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--directory', dest='directory', type=str, help='Path to directory of images')
+    parser.add_argument('--test_directory', dest='test_directory', type=str, help='Path to directory of test images')
     parser.add_argument('--initial_epochs', dest='initial_epochs', type=int, help='Number of initial epochs',
                         default=5)
     parser.add_argument('--fine_tune_epochs', dest='fine_tune_epochs', type=int, help='Number of fine tuning epochs',
                         default=5)
     parser.add_argument('--base_lr', dest='base_lr', type=float, help='Base learning rate',
                         default=0.001)
+    parser.add_argument('--validation_split', dest='validation_split', type=float,
+                        help='Fraction of data reserved for validation',
+                        default=0.2)
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = get_args()
     directory = args.directory
+    test_directory = args.test_directory
     initial_epochs = args.initial_epochs
     fine_tune_epochs = args.fine_tune_epochs
     base_lr = args.base_lr
     img_size = (160, 160)
     batch_size = 32
-    run(directory, img_size, batch_size, initial_epochs, fine_tune_epochs, base_lr)
+    validation_split = args.validation_split
+    run(directory, test_directory, img_size, batch_size, initial_epochs, fine_tune_epochs, base_lr, validation_split)
 
